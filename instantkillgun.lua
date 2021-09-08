560b2dff87c0bca0afff11674e33c327c0522a25
HudsonDuke19 <90293634+HudsonDuke19@users.noreply.github.com>
2021-09-08 11:13:12 +1000

    Update instantkillgun.lua

--- instantkillgun.lua
+++ instantkillgun.lua
@@ -2,6 +2,6 @@ local plr = game.Players.LocalPlayer
 local killgun = plr.Backpack.M9.GunStates
 
 a = require(killgun)
-a.Damage = math.huge
+a.Damage = 1000
 a.MaxAmmo = math.huge
 a.CurrentAmmo = math.huge
